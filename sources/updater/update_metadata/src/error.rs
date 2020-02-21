c06e0a978dd8ebe0965a5d7eedde1471eeb06925
Tom Kirchner <tjk@amazon.com>
2020-02-21 14:51:39 -0800

    Rename workspaces directory to 'sources', package to 'os'
    workspaces was originally a set of Cargo workspaces, but we now build them
    together because it's much faster to build.  This renames the 'workspaces'
    directory to the clearer 'sources' because it contains our source code.  The
    'workspaces' package is renamed to 'os' to represent that it contains our
    top-level / userspace OS components.

---
+++ sources/updater/update_metadata/src/error.rs
@@ -0,0 +1,108 @@
+#![allow(clippy::default_trait_access)]
+
+use semver::Version;
+use snafu::{Backtrace, Snafu};
+use std::path::PathBuf;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Snafu)]
+#[snafu(visibility = "pub(crate)")]
+pub enum Error {
+    #[snafu(display("Bad bound field: {}", bound_str))]
+    BadBound {
+        backtrace: Backtrace,
+        source: std::num::ParseIntError,
+        bound_str: String,
+    },
+
+    #[snafu(display("Invalid bound start: {}", key))]
+    BadBoundKey {
+        source: std::num::ParseIntError,
+        key: String,
+        backtrace: Backtrace,
+    },
+
+    #[snafu(display("Could not parse OS version: {}", key))]
+    BadVersion {
+        backtrace: Backtrace,
+        key: String,
+        source: semver::SemVerError,
+    },
+
+    #[snafu(display("Could not parse OS versions: {}", key))]
+    BadDataVersionsFromTo { backtrace: Backtrace, key: String },
+
+    #[snafu(display("Could not parse image version: {} - {}", key, value))]
+    BadMapVersion {
+        backtrace: Backtrace,
+        key: String,
+        value: String,
+    },
+
+    #[snafu(display("Migration {} matches regex but missing version", name))]
+    BadRegexVersion { name: String },
+
+    #[snafu(display("Migration {} matches regex but missing name", name))]
+    BadRegexName { name: String },
+
+    #[snafu(display("Duplicate key ID: {}", keyid))]
+    DuplicateKeyId { backtrace: Backtrace, keyid: u32 },
+
+    #[snafu(display("Duplicate version key: {}", key))]
+    DuplicateVersionKey { backtrace: Backtrace, key: String },
+
+    #[snafu(display("Failed to parse updates manifest: {}", source))]
+    ManifestParse {
+        source: serde_json::Error,
+        backtrace: Backtrace,
+    },
+
+    #[snafu(display("Failed to read manifest file {}: {}", path.display(), source))]
+    ManifestRead {
+        path: PathBuf,
+        source: std::io::Error,
+        backtrace: Backtrace,
+    },
+
+    #[snafu(display("Failed to write manifest file {}: {}", path.display(), source))]
+    ManifestWrite {
+        path: PathBuf,
+        source: std::io::Error,
+        backtrace: Backtrace,
+    },
+
+    #[snafu(display(
+        "Migration {} given for {} but name implies it is for {}",
+        name,
+        to,
+        version
+    ))]
+    MigrationInvalidTarget {
+        backtrace: Backtrace,
+        name: String,
+        to: Version,
+        version: Version,
+    },
+
+    #[snafu(display(
+        "Migration name invalid; must follow format 'migrate_${{TO_VERSION}}_${{NAME}}'"
+    ))]
+    MigrationNaming { backtrace: Backtrace },
+
+    #[snafu(display("Unable to get mutable reference to ({},{}) migrations", from, to))]
+    MigrationMutable {
+        backtrace: Backtrace,
+        from: Version,
+        to: Version,
+    },
+
+    #[snafu(display("Failed to serialize update information: {}", source))]
+    UpdateSerialize {
+        source: serde_json::Error,
+        backtrace: Backtrace,
+    },
+
+    #[snafu(display("Waves are not ordered: bound {} occurs before bound {}", next, wave))]
+    WavesUnordered { wave: u32, next: u32 },
+}
---
+++ sources/updater/update_metadata/src/se.rs
@@ -0,0 +1,30 @@
+use semver::Version;
+use serde::ser::Error as _;
+use serde::{Serialize, Serializer};
+use std::collections::BTreeMap;
+
+pub(crate) fn serialize_migration<S>(
+    value: &BTreeMap<(Version, Version), Vec<String>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut map = BTreeMap::new();
+    for ((from, to), val) in value {
+        // NOTE: The space in this tuple is required for versions of Bottlerocket < 0.2.0
+        let key = format!(
+            "({}, {})",
+            serde_plain::to_string(&from).map_err(|e| S::Error::custom(format!(
+                "Could not serialize 'from' version: {}",
+                e
+            )))?,
+            serde_plain::to_string(&to).map_err(|e| S::Error::custom(format!(
+                "Could not serialize 'to' version: {}",
+                e
+            )))?,
+        );
+        map.insert(key, val);
+    }
+    map.serialize(serializer)
+}
