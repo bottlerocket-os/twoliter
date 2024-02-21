bottlerocket-os/twoliter

commit 92bc3bbe40b816b554020f90aa37542f1ca3ec51

buildsys: refactor builder.rs
Refactor the code, primarily in builder.rs. The goal of the refactor is
to segregate data collection and docker argument creation from program
flow logic.

tools/buildsys/src/main.rs
@@ -16,7 +16,7 @@ mod project;
 mod spec;
 
 use crate::args::{BuildPackageArgs, BuildVariantArgs, Buildsys, Command};
-use builder::{PackageBuilder, VariantBuilder};
+use crate::builder::DockerBuild;
 use buildsys::manifest::{BundleModule, ManifestInfo, SupportedArch};
 use cache::LookasideCache;
 use clap::Parser;
@@ -28,6 +28,7 @@ use std::path::PathBuf;
 use std::process;
 
 mod error {
+    use buildsys::manifest::SupportedArch;
     use snafu::Snafu;
 
     #[derive(Debug, Snafu)]
@@ -75,7 +76,7 @@ mod error {
             supported_arches.join(", ")
         ))]
         UnsupportedArch {
-            arch: String,
+            arch: SupportedArch,
             supported_arches: Vec<String>,
         },
     }
@@ -114,7 +115,7 @@ fn build_package(args: BuildPackageArgs) -> Result<()> {
         .join(manifest_file);
     let variant_manifest =
         ManifestInfo::new(variant_manifest_path).context(error::ManifestParseSnafu)?;
-    supported_arch(&variant_manifest, &args.common.arch)?;
+    supported_arch(&variant_manifest, args.common.arch)?;
     let mut image_features = variant_manifest.image_features();
 
     let manifest = ManifestInfo::new(args.common.cargo_manifest_dir.join(manifest_file))
@@ -224,8 +225,10 @@ fn build_package(args: BuildPackageArgs) -> Result<()> {
         println!("cargo:rerun-if-changed={}", f.display());
     }
 
-    PackageBuilder::build(&args, &package, image_features).context(error::BuildAttemptSnafu)?;
-
+    DockerBuild::new_package(args, &manifest)
+        .unwrap()
+        .build()
+        .context(error::BuildAttemptSnafu)?;
     Ok(())
 }
 
@@ -236,37 +239,24 @@ fn build_variant(args: BuildVariantArgs) -> Result<()> {
     let manifest = ManifestInfo::new(args.common.cargo_manifest_dir.join(manifest_file))
         .context(error::ManifestParseSnafu)?;
 
-    supported_arch(&manifest, &args.common.arch)?;
+    supported_arch(&manifest, args.common.arch)?;
 
-    if let Some(packages) = manifest.included_packages() {
-        let image_format = manifest.image_format();
-        let image_layout = manifest.image_layout();
-        let kernel_parameters = manifest.kernel_parameters();
-        let image_features = manifest.image_features();
-        VariantBuilder::build(
-            &args,
-            packages,
-            image_format,
-            image_layout,
-            kernel_parameters,
-            image_features,
-        )
-        .context(error::BuildAttemptSnafu)?;
+    if manifest.included_packages().is_some() {
+        DockerBuild::new_variant(args, &manifest)
+            .unwrap()
+            .build()
+            .context(error::BuildAttemptSnafu)?;
     } else {
         println!("cargo:warning=No included packages in manifest. Skipping variant build.");
     }
-
     Ok(())
 }
 
 /// Ensure that the current arch is supported by the current variant
-fn supported_arch(manifest: &ManifestInfo, arch: &str) -> Result<()> {
+fn supported_arch(manifest: &ManifestInfo, arch: SupportedArch) -> Result<()> {
     if let Some(supported_arches) = manifest.supported_arches() {
-        let current_arch: SupportedArch =
-            serde_plain::from_str(arch).context(error::UnknownArchSnafu { arch })?;
-
         ensure!(
-            supported_arches.contains(&current_arch),
+            supported_arches.contains(&arch),
             error::UnsupportedArchSnafu {
                 arch,
                 supported_arches: supported_arches

tools/buildsys/src/manifest.rs
@@ -237,7 +237,7 @@ uefi-secure-boot = true
 
 mod error;
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use snafu::{ResultExt, Snafu};
 use std::cmp::max;
 use std::collections::{HashMap, HashSet};
@@ -501,12 +501,15 @@ pub enum PartitionPlan {
     Unified,
 }
 
-#[derive(Deserialize, Debug, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Serialize, Debug, Copy, Clone, PartialEq, Eq, Hash)]
 #[serde(rename_all = "lowercase")]
 pub enum SupportedArch {
     X86_64,
     Aarch64,
 }
+
+serde_plain::derive_fromstr_from_deserialize!(SupportedArch);
+serde_plain::derive_display_from_serialize!(SupportedArch);
 
 /// Map a Linux architecture into the corresponding Docker architecture.
 impl SupportedArch {
@@ -518,7 +521,7 @@ impl SupportedArch {
     }
 }
 
-#[derive(Deserialize, Debug, PartialEq, Eq, Hash)]
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[serde(try_from = "String")]
 pub enum ImageFeature {
     GrubSetPrivateVar,
@@ -570,13 +573,4 @@ pub struct ExternalFile {
     pub bundle_modules: Option<Vec<BundleModule>>,
     pub bundle_root_path: Option<PathBuf>,
     pub bundle_output_path: Option<PathBuf>,
-}
-
-impl fmt::Display for SupportedArch {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            SupportedArch::X86_64 => write!(f, "x86_64"),
-            SupportedArch::Aarch64 => write!(f, "aarch64"),
-        }
-    }
 }

tools/buildsys/src/args.rs
@@ -5,6 +5,7 @@ of its input arguments from environment variables.
 
 !*/
 
+use buildsys::manifest::SupportedArch;
 use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 use url::Url;
@@ -55,10 +56,10 @@ impl Command {
 #[derive(Debug, Parser)]
 pub(crate) struct Common {
     #[arg(long, env = "BUILDSYS_ARCH")]
-    pub(crate) arch: String,
+    pub(crate) arch: SupportedArch,
 
     #[arg(long, env = "BUILDSYS_OUTPUT_DIR")]
-    pub(crate) output_dir: PathBuf,
+    pub(crate) image_arch_variant_dir: PathBuf,
 
     #[arg(long, env = "BUILDSYS_ROOT_DIR")]
     pub(crate) root_dir: PathBuf,
@@ -80,6 +81,9 @@ pub(crate) struct Common {
 
     #[arg(long, env = "TLPRIVATE_TOOLCHAIN")]
     pub(crate) toolchain: String,
+
+    #[arg(long, env = "TWOLITER_TOOLS_DIR")]
+    pub(crate) tools_dir: PathBuf,
 }
 
 /// Build RPMs from a spec file and sources.
@@ -90,6 +94,21 @@ pub(crate) struct BuildPackageArgs {
 
     #[arg(long, env = "BUILDSYS_VARIANT")]
     pub(crate) variant: String,
+
+    #[arg(long, env = "BUILDSYS_VARIANT_PLATFORM")]
+    pub(crate) variant_platform: String,
+
+    #[arg(long, env = "BUILDSYS_VARIANT_RUNTIME")]
+    pub(crate) variant_runtime: String,
+
+    #[arg(long, env = "BUILDSYS_VARIANT_FAMILY")]
+    pub(crate) variant_family: String,
+
+    #[arg(long, env = "BUILDSYS_VARIANT_FLAVOR")]
+    pub(crate) variant_flavor: String,
+
+    #[arg(long, env = "PUBLISH_REPO")]
+    pub(crate) publish_repo: String,
 
     #[arg(long, env = "BUILDSYS_SOURCES_DIR")]
     pub(crate) sources_dir: PathBuf,

commit 21691cf468165fa4a68d771d342f64fa0b783663

buildsys: fix image features
This commt fixes a couple of regressions introduced in 92bc3bb. Most
notably, the wrong set of image_features was being used to set package
build args. In this commit we correctly use the image features found
in the downstream variants Cargo.toml.

Also fixes an oversight where an error constructing the Builder object
would have resulted in a panic.

tools/buildsys/src/builder.rs
@@ -114,7 +114,12 @@ impl CommonBuildArgs {
 }
 
 struct PackageBuildArgs {
-    image_features: Option<HashSet<ImageFeature>>,
+    /// The package might need to know what the `image_features` are going to be for the variant
+    /// it is going to be used in downstream. This is because certain packages will be built
+    /// differently based on certain image features such as cgroupsv1 vs cgroupsv2. During a
+    /// package build, these are determined by looking at the variant's Cargo.toml file based on
+    /// what was found in `BUILDSYS_VARIANT`.
+    image_features: HashSet<ImageFeature>,
     package: String,
     publish_repo: String,
     variant: String,
@@ -134,11 +139,10 @@ impl PackageBuildArgs {
         args.build_arg("VARIANT_FLAVOR", &self.variant_flavor);
         args.build_arg("VARIANT_PLATFORM", &self.variant_platform);
         args.build_arg("VARIANT_RUNTIME", &self.variant_runtime);
-        if let Some(image_features) = &self.image_features {
-            for image_feature in image_features.iter() {
-                args.build_arg(format!("{}", image_feature), "1");
-            }
+        for image_feature in &self.image_features {
+            args.build_arg(format!("{}", image_feature), "1");
         }
+
         args
     }
 }
@@ -218,7 +222,11 @@ pub(crate) struct DockerBuild {
 
 impl DockerBuild {
     /// Create a new `DockerBuild` that can build a package.
-    pub(crate) fn new_package(args: BuildPackageArgs, manifest: &ManifestInfo) -> Result<Self> {
+    pub(crate) fn new_package(
+        args: BuildPackageArgs,
+        manifest: &ManifestInfo,
+        image_features: HashSet<ImageFeature>,
+    ) -> Result<Self> {
         let package = if let Some(name_override) = manifest.package_name() {
             name_override.clone()
         } else {
@@ -248,9 +256,7 @@ impl DockerBuild {
                 args.common.arch,
             ),
             target_build_args: TargetBuildArgs::Package(PackageBuildArgs {
-                image_features: manifest
-                    .image_features()
-                    .map(|set| set.iter().map(|&x| x.to_owned()).collect()),
+                image_features,
                 package,
                 publish_repo: args.publish_repo,
                 variant: args.variant,
@@ -301,12 +307,7 @@ impl DockerBuild {
             target_build_args: TargetBuildArgs::Variant(VariantBuildArgs {
                 data_image_publish_size_gib,
                 data_image_size_gib: data_image_size_gib.to_string(),
-                image_features: manifest
-                    .image_features()
-                    .unwrap_or_default()
-                    .into_iter()
-                    .copied()
-                    .collect(),
+                image_features: manifest.image_features().unwrap_or_default(),
                 image_format: match manifest.image_format() {
                     Some(ImageFormat::Raw) | None => "raw",
                     Some(ImageFormat::Qcow2) => "qcow2",

tools/buildsys/src/main.rs
@@ -58,6 +58,11 @@ mod error {
             source: super::builder::error::Error,
         },
 
+        #[snafu(display("Unable to instantiate the builder: {source}"))]
+        BuilderInstantiation {
+            source: crate::builder::error::Error,
+        },
+
         #[snafu(display("Missing environment variable '{}'", var))]
         Environment {
             var: String,
@@ -225,8 +230,8 @@ fn build_package(args: BuildPackageArgs) -> Result<()> {
         println!("cargo:rerun-if-changed={}", f.display());
     }
 
-    DockerBuild::new_package(args, &manifest)
-        .unwrap()
+    DockerBuild::new_package(args, &manifest, image_features.unwrap_or_default())
+        .context(error::BuilderInstantiationSnafu)?
         .build()
         .context(error::BuildAttemptSnafu)?;
     Ok(())
@@ -243,7 +248,7 @@ fn build_variant(args: BuildVariantArgs) -> Result<()> {
 
     if manifest.included_packages().is_some() {
         DockerBuild::new_variant(args, &manifest)
-            .unwrap()
+            .context(error::BuilderInstantiationSnafu)?
             .build()
             .context(error::BuildAttemptSnafu)?;
     } else {

tools/buildsys/src/manifest.rs
@@ -325,11 +325,11 @@ impl ManifestInfo {
     }
 
     /// Convenience method to return the enabled image features for this variant.
-    pub fn image_features(&self) -> Option<HashSet<&ImageFeature>> {
+    pub fn image_features(&self) -> Option<HashSet<ImageFeature>> {
         self.build_variant().and_then(|b| {
             b.image_features
                 .as_ref()
-                .map(|m| m.iter().filter(|(_k, v)| **v).map(|(k, _v)| k).collect())
+                .map(|m| m.iter().filter(|(_k, v)| **v).map(|(k, _v)| *k).collect())
         })
     }
 

tools/pubsys/src/aws/ami/register.rs
@@ -129,7 +129,7 @@ async fn _register_image(
         .image_features()
         .iter()
         .flatten()
-        .any(|f| **f == ImageFeature::UefiSecureBoot);
+        .any(|f| *f == ImageFeature::UefiSecureBoot);
 
     let (boot_mode, uefi_data) = if uefi_secure_boot_enabled {
         (Some("uefi-preferred".into()), Some(uefi_data))