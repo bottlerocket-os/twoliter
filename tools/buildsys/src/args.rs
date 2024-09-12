bottlerocket-os/twoliter

commit 4526d6eeb9330dabe9793fb4f8702be459a95b0e

buildsys: stop relying on BUILDSYS_VARIANT
When building all variants, `cargo` will start many builds at once,
which won't match up with the value in the environment variable.

Instead, use CARGO_MANIFEST_DIR to determine which variant is being
built. Parse that into its subcomponents at runtime to avoid needing
any of the other variant-related environment variables.

Signed-off-by: Ben Cressey <bcressey@amazon.com>

tools/buildsys/src/args.rs
@@ -21,7 +21,7 @@ const REBUILD_VARS: [(&str, u8); 15] = [
     ("BUILDSYS_KITS_DIR", KIT),
     ("BUILDSYS_EXTERNAL_KITS_DIR", PACKAGE | KIT | VARIANT),
     ("BUILDSYS_NAME", VARIANT),
-    ("BUILDSYS_OUTPUT_DIR", VARIANT),
+    ("BUILDSYS_IMAGES_DIR", VARIANT),
     ("BUILDSYS_OUTPUT_GENERATION_ID", PACKAGE | KIT | VARIANT),
     ("BUILDSYS_PACKAGES_DIR", PACKAGE),
     ("BUILDSYS_PRETTY_NAME", VARIANT),
@@ -67,9 +67,6 @@ pub(crate) struct Common {
 
     #[arg(long, env = "BUILDSYS_CARGO_METADATA_PATH")]
     pub(crate) cargo_metadata_path: PathBuf,
-
-    #[arg(long, env = "BUILDSYS_OUTPUT_DIR")]
-    pub(crate) image_arch_variant_dir: PathBuf,
 
     #[arg(long, env = "BUILDSYS_ROOT_DIR")]
     pub(crate) root_dir: PathBuf,
@@ -157,26 +154,17 @@ pub(crate) struct BuildVariantArgs {
     #[arg(long, env = "BUILDSYS_PRETTY_NAME")]
     pub(crate) pretty_name: String,
 
-    #[arg(long, env = "BUILDSYS_VARIANT")]
-    pub(crate) variant: String,
-
-    #[arg(long, env = "BUILDSYS_VARIANT_PLATFORM")]
-    pub(crate) variant_platform: String,
-
-    #[arg(long, env = "BUILDSYS_VARIANT_RUNTIME")]
-    pub(crate) variant_runtime: String,
-
-    #[arg(long, env = "BUILDSYS_VARIANT_FAMILY")]
-    pub(crate) variant_family: String,
-
-    #[arg(long, env = "BUILDSYS_VARIANT_FLAVOR")]
-    pub(crate) variant_flavor: String,
+    #[arg(long, env = "CARGO_MANIFEST_DIR")]
+    pub(crate) variant: PathBuf,
 
     #[arg(long, env = "BUILDSYS_VERSION_BUILD")]
     pub(crate) version_build: String,
 
     #[arg(long, env = "BUILDSYS_VERSION_IMAGE")]
     pub(crate) version_image: String,
+
+    #[arg(long, env = "BUILDSYS_IMAGES_DIR")]
+    pub(crate) image_dir: PathBuf,
 
     #[command(flatten)]
     pub(crate) common: Common,
@@ -188,14 +176,17 @@ pub(crate) struct RepackVariantArgs {
     #[arg(long, env = "BUILDSYS_NAME")]
     pub(crate) name: String,
 
-    #[arg(long, env = "BUILDSYS_VARIANT")]
-    pub(crate) variant: String,
+    #[arg(long, env = "CARGO_MANIFEST_DIR")]
+    pub(crate) variant: PathBuf,
 
     #[arg(long, env = "BUILDSYS_VERSION_BUILD")]
     pub(crate) version_build: String,
 
     #[arg(long, env = "BUILDSYS_VERSION_IMAGE")]
     pub(crate) version_image: String,
+
+    #[arg(long, env = "BUILDSYS_IMAGES_DIR")]
+    pub(crate) image_dir: PathBuf,
 
     #[command(flatten)]
     pub(crate) common: Common,

tools/buildsys/src/builder.rs
@@ -7,6 +7,7 @@ the repository's top-level Dockerfile.
 pub(crate) mod error;
 
 use crate::args::{BuildKitArgs, BuildPackageArgs, BuildVariantArgs, RepackVariantArgs};
+use bottlerocket_variant::Variant;
 use buildsys::manifest::{
     ExternalKitMetadataView, ImageFeature, ImageFormat, ImageLayout, Manifest, PartitionPlan,
     SupportedArch,
@@ -419,22 +420,28 @@ impl DockerBuild {
         let (os_image_publish_size_gib, data_image_publish_size_gib) =
             image_layout.publish_image_sizes_gib();
 
+        let variant = filename(args.variant);
+
+        let v = Variant::new(&variant).context(error::VariantParseSnafu)?;
+        let variant_platform = v.platform().into();
+        let variant_runtime = v.runtime().into();
+        let variant_family = v.family().into();
+        let variant_flavor = v.variant_flavor().unwrap_or("").into();
+
         Ok(Self {
             dockerfile: args.common.tools_dir.join("build.Dockerfile"),
             context: args.common.root_dir.clone(),
             target: "variant".to_string(),
             tag: append_token(
-                format!(
-                    "buildsys-var-{variant}-{arch}",
-                    variant = args.variant,
-                    arch = args.common.arch
-                ),
+                format!("buildsys-var-{variant}-{arch}", arch = args.common.arch),
                 &args.common.root_dir,
             ),
             root_dir: args.common.root_dir.clone(),
-            artifacts_dirs: vec![args.common.image_arch_variant_dir],
+            artifacts_dirs: vec![args
+                .image_dir
+                .join(format!("{}-{}", args.common.arch, variant))],
             state_dir: args.common.state_dir,
-            artifact_name: args.variant.clone(),
+            artifact_name: variant.clone(),
             common_build_args: CommonBuildArgs::new(
                 &args.common.root_dir,
                 args.common.sdk_image,
@@ -477,11 +484,11 @@ impl DockerBuild {
                 }
                 .to_string(),
                 pretty_name: args.pretty_name,
-                variant: args.variant,
-                variant_family: args.variant_family,
-                variant_flavor: args.variant_flavor,
-                variant_platform: args.variant_platform,
-                variant_runtime: args.variant_runtime,
+                variant,
+                variant_family,
+                variant_flavor,
+                variant_platform,
+                variant_runtime,
                 version_build: args.version_build,
                 version_image: args.version_image,
             }),
@@ -502,22 +509,22 @@ impl DockerBuild {
         let (os_image_publish_size_gib, data_image_publish_size_gib) =
             image_layout.publish_image_sizes_gib();
 
+        let variant = filename(args.variant);
+
         Ok(Self {
             dockerfile: args.common.tools_dir.join("build.Dockerfile"),
             context: args.common.root_dir.clone(),
             target: "repack".to_string(),
             tag: append_token(
-                format!(
-                    "buildsys-repack-{variant}-{arch}",
-                    variant = args.variant,
-                    arch = args.common.arch
-                ),
+                format!("buildsys-repack-{variant}-{arch}", arch = args.common.arch),
                 &args.common.root_dir,
             ),
             root_dir: args.common.root_dir.clone(),
-            artifacts_dirs: vec![args.common.image_arch_variant_dir],
+            artifacts_dirs: vec![args
+                .image_dir
+                .join(format!("{}-{}", args.common.arch, variant))],
             state_dir: args.common.state_dir,
-            artifact_name: args.variant.clone(),
+            artifact_name: variant.clone(),
             common_build_args: CommonBuildArgs::new(
                 &args.common.root_dir,
                 args.common.sdk_image,
@@ -542,7 +549,7 @@ impl DockerBuild {
                     PartitionPlan::Unified => "unified",
                 }
                 .to_string(),
-                variant: args.variant,
+                variant,
                 version_build: args.version_build,
                 version_image: args.version_image,
             }),
@@ -1035,4 +1042,18 @@ where
     fn split_string(&self) -> Vec<String> {
         self.as_ref().split(' ').map(String::from).collect()
     }
+}
+
+// =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=
+
+/// Helper to extract the file name from a path.
+fn filename(p: impl AsRef<Path>) -> String {
+    let path = p.as_ref();
+    path.file_name()
+        .with_context(|| error::BadFilenameSnafu {
+            path: PathBuf::from(path),
+        })
+        .unwrap()
+        .to_string_lossy()
+        .to_string()
 }

tools/buildsys/src/builder/error.rs
@@ -10,6 +10,9 @@ pub(crate) enum Error {
     #[snafu(display("Failed to read CA certificates bundle '{}'", ca_bundle_path.display()))]
     BadCaBundle { ca_bundle_path: PathBuf },
 
+    #[snafu(display("Failed to get file name for '{}'", path.display()))]
+    BadFilename { path: PathBuf },
+
     #[snafu(display("Failed to read repo root '{}'", root_json_path.display()))]
     BadRootJson { root_json_path: PathBuf },
 
@@ -82,6 +85,11 @@ pub(crate) enum Error {
         path: PathBuf,
         prefix: PathBuf,
         source: std::path::StripPrefixError,
+    },
+
+    #[snafu(display("Failed to parse variant: {source}"))]
+    VariantParse {
+        source: bottlerocket_variant::error::Error,
     },
 }
 

commit 1cdb3029417dddab97dfbc6980041e785c68f2ee

buildsys: drop variant arg for variant subcommands
The switch from relying on BUILDSYS_VARIANT to CARGO_MANIFEST_DIR did
not take into account the case where `buildsys` is not executed by
`cargo`, and hence CARGO_MANIFEST_DIR is not set in the environment.

This broke the repack-variant subcommand, which receives the value
from a command-line argument when invoked through Makefile.toml.

In any case, having both `variant` and `cargo-manifest-dir` args that
are controlled by the same environment variable is redundant. Fix the
bug and remove the confusion by using `cargo-manifest-dir` alone to
identify the artifact that `buildsys` is meant to act on.

Fixes: 4526d6ee ("buildsys: stop relying on BUILDSYS_VARIANT")

Signed-off-by: Ben Cressey <bcressey@amazon.com>

tools/buildsys/src/args.rs
@@ -162,9 +162,6 @@ pub(crate) struct BuildVariantArgs {
     #[arg(long, env = "BUILDSYS_PRETTY_NAME")]
     pub(crate) pretty_name: String,
 
-    #[arg(long, env = "CARGO_MANIFEST_DIR")]
-    pub(crate) variant: PathBuf,
-
     #[arg(long, env = "BUILDSYS_VERSION_BUILD")]
     pub(crate) version_build: String,
 
@@ -183,9 +180,6 @@ pub(crate) struct BuildVariantArgs {
 pub(crate) struct RepackVariantArgs {
     #[arg(long, env = "BUILDSYS_NAME")]
     pub(crate) name: String,
-
-    #[arg(long, env = "CARGO_MANIFEST_DIR")]
-    pub(crate) variant: PathBuf,
 
     #[arg(long, env = "BUILDSYS_VERSION_BUILD")]
     pub(crate) version_build: String,

tools/buildsys/src/builder.rs
@@ -420,7 +420,7 @@ impl DockerBuild {
         let (os_image_publish_size_gib, data_image_publish_size_gib) =
             image_layout.publish_image_sizes_gib();
 
-        let variant = filename(args.variant);
+        let variant = filename(args.common.cargo_manifest_dir);
 
         let v = Variant::new(&variant).context(error::VariantParseSnafu)?;
         let variant_platform = v.platform().into();
@@ -509,7 +509,7 @@ impl DockerBuild {
         let (os_image_publish_size_gib, data_image_publish_size_gib) =
             image_layout.publish_image_sizes_gib();
 
-        let variant = filename(args.variant);
+        let variant = filename(args.common.cargo_manifest_dir);
 
         Ok(Self {
             dockerfile: args.common.tools_dir.join("build.Dockerfile"),