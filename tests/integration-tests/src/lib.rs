b8c8547a8a6bafd6a69d2a83ae8b3d8d95722622
Sean P. Kelly <seankell@amazon.com>
2024-10-07 06:33:49 +0000

    integ: run push and pull from local registry when testing
    Co-authored-by: Jarrett Tierney <jmt@amazon.com>

--- tests/integration-tests/src/lib.rs
+++ tests/integration-tests/src/lib.rs
@@ -2,7 +2,8 @@
 
 use std::ffi::OsStr;
 use std::path::PathBuf;
-use tokio::process::Command;
+use std::process::Command;
+use tempfile::TempDir;
 
 mod twoliter_update;
 
@@ -14,7 +15,7 @@ pub fn test_projects_dir() -> PathBuf {
     p.join("projects")
 }
 
-pub async fn run_command<I, S, E>(cmd: S, args: I, env: E) -> std::process::Output
+pub fn run_command<I, S, E>(cmd: S, args: I, env: E) -> std::process::Output
 where
     I: IntoIterator<Item = S>,
     E: IntoIterator<Item = (S, S)>,
@@ -35,10 +36,88 @@ where
         .args(args.into_iter())
         .envs(env.into_iter())
         .output()
-        .await
         .expect("failed to execute process");
 
     println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
     println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
     output
+}
+
+struct KitRegistry {
+    temp_dir: TempDir,
+    container_id: String,
+}
+
+impl KitRegistry {
+    fn new() -> Self {
+        let temp_dir = TempDir::new().expect("failed to create path for oci registry spinup");
+
+        let cert_dir = temp_dir.path().join("certs");
+        let cert_file = cert_dir.join("registry.crt");
+        std::fs::create_dir_all(&cert_dir).expect("failed to create nginx dir");
+        let output = run_command(
+            "openssl",
+            [
+                "req",
+                "-x509",
+                "-nodes",
+                "-days",
+                "365",
+                "-newkey",
+                "rsa:2048",
+                "-keyout",
+                cert_dir.join("registry.key").to_str().unwrap(),
+                "-out",
+                cert_file.to_str().unwrap(),
+                "-batch",
+                "-addext",
+                "subjectAltName=DNS:localhost",
+            ],
+            [],
+        );
+        assert!(
+            output.status.success(),
+            "generate openssl self-signed certificates"
+        );
+
+        let output = run_command(
+            "docker",
+            [
+                "run",
+                "-d",
+                "--rm",
+                "--volume",
+                "./certs:/auth/certs",
+                "-e REGISTRY_HTTP_RELATIVEURLS=true",
+                "-e REGISTRY_HTTP_ADDR=0.0.0.0:5000",
+                "-e REGISTRY_HTTP_TLS_CERTIFICATE=/auth/certs/registry.crt",
+                "-e REGISTRY_HTTP_TLS_KEY=/auth/certs/registry.key",
+                "-p",
+                "5000:5000",
+                "public.ecr.aws/docker/library/registry:2.8.3",
+            ],
+            [],
+        );
+        assert!(output.status.success(), "failed to start oci registry");
+        let container_id = String::from_utf8(output.stdout).unwrap().trim().to_string();
+
+        Self {
+            temp_dir,
+            container_id,
+        }
+    }
+
+    fn cert_file(&self) -> PathBuf {
+        self.temp_dir
+            .path()
+            .join("certs/registry.crt")
+            .to_path_buf()
+    }
+}
+
+impl Drop for KitRegistry {
+    fn drop(&mut self) {
+        let output = run_command("docker", ["kill", &self.container_id], []);
+        assert!(output.status.success(), "failed to stop oci registry");
+    }
 }
--- tests/integration-tests/src/twoliter_update.rs
+++ tests/integration-tests/src/twoliter_update.rs
@@ -1,59 +1,32 @@
-use super::{run_command, test_projects_dir, TWOLITER_PATH};
+use super::{run_command, test_projects_dir, KitRegistry, TWOLITER_PATH};
 
-const EXPECTED_LOCKFILE: &str = r#"schema-version = 1
-
-[sdk]
-name = "bottlerocket-sdk"
-version = "0.42.0"
-vendor = "bottlerocket"
-source = "public.ecr.aws/bottlerocket/bottlerocket-sdk:v0.42.0"
-digest = "myHHKE41h9qfeyR6V6HB0BfiLPwj3QEFLUFy4TXcR10="
-
-[[kit]]
-name = "bottlerocket-core-kit"
-version = "2.0.0"
-vendor = "custom-vendor"
-source = "public.ecr.aws/bottlerocket/bottlerocket-core-kit:v2.0.0"
-digest = "vlTsAAbSCzXFZofVmw8pLLkRjnG/y8mtb2QsQBSz1zk="
+const INFRA_TOML: &str = r#"
+[vendor.bottlerocket]
+registry = "localhost:5000"
 "#;
 
-#[tokio::test]
-#[ignore]
-/// Generates a Twoliter.lock file for the `external-kit` project using docker
-async fn test_twoliter_update_docker() {
-    let external_kit = test_projects_dir().join("external-kit");
+const TWOLITER_OVERRIDE: &str = r#"
+[custom-vendor.core-kit]
+registry = "localhost:5000"
+name = "core-kit-overridden"
+"#;
 
-    let lockfile = external_kit.join("Twoliter.lock");
-    tokio::fs::remove_file(&lockfile).await.ok();
-
-    let output = run_command(
-        TWOLITER_PATH,
-        [
-            "update",
-            "--project-path",
-            external_kit.join("Twoliter.toml").to_str().unwrap(),
-        ],
-        [("TWOLITER_KIT_IMAGE_TOOL", "docker")],
-    )
-    .await;
-
-    assert!(output.status.success());
-
-    let lock_contents = tokio::fs::read_to_string(&lockfile).await.unwrap();
-    assert_eq!(lock_contents, EXPECTED_LOCKFILE);
-
-    tokio::fs::remove_file(&lockfile).await.ok();
-}
-
-#[tokio::test]
+#[test]
 #[ignore]
 /// Generates a Twoliter.lock file for the `external-kit` project using crane
-async fn test_twoliter_update_crane() {
+fn test_twoliter_build_and_update() {
     let external_kit = test_projects_dir().join("external-kit");
-
     let lockfile = external_kit.join("Twoliter.lock");
-    tokio::fs::remove_file(&lockfile).await.ok();
+    std::fs::remove_file(&lockfile).ok();
+    let override_file = external_kit.join("Twoliter.override");
+    std::fs::remove_file(&override_file).ok();
 
+    // Build & push a local kit to the registry
+    let registry = KitRegistry::new();
+    LocalKit::build(&registry);
+
+    // Point twoliter to the local registry as an override
+    std::fs::write(&override_file, TWOLITER_OVERRIDE).unwrap();
     let output = run_command(
         TWOLITER_PATH,
         [
@@ -61,14 +34,101 @@ async fn test_twoliter_update_crane() {
             "--project-path",
             external_kit.join("Twoliter.toml").to_str().unwrap(),
         ],
-        [("TWOLITER_KIT_IMAGE_TOOL", "crane")],
-    )
-    .await;
+        [
+            ("TWOLITER_KIT_IMAGE_TOOL", "crane"),
+            ("SSL_CERT_FILE", registry.cert_file().to_str().unwrap()),
+        ],
+    );
 
     assert!(output.status.success());
 
-    let lock_contents = tokio::fs::read_to_string(&lockfile).await.unwrap();
-    assert_eq!(lock_contents, EXPECTED_LOCKFILE);
+    // Assert that we successfully create a lock
+    let lock_contents = std::fs::read_to_string(&lockfile).unwrap();
+    let parsed: toml::Value = toml::from_str(&lock_contents).unwrap();
+    let kits = parsed
+        .as_table()
+        .unwrap()
+        .get("kit")
+        .unwrap()
+        .as_array()
+        .unwrap();
 
-    tokio::fs::remove_file(&lockfile).await.ok();
+    assert_eq!(kits.len(), 1);
+    let core_kit = kits[0].as_table().unwrap();
+    assert_eq!(core_kit.get("name").unwrap().as_str().unwrap(), "core-kit");
+    assert_eq!(core_kit.get("version").unwrap().as_str().unwrap(), "1.0.0");
+    assert_eq!(
+        core_kit.get("vendor").unwrap().as_str().unwrap(),
+        "custom-vendor"
+    );
+    assert_eq!(
+        core_kit.get("source").unwrap().as_str().unwrap(),
+        "definitely-wont-resolve/core-kit:v1.0.0"
+    );
+
+    std::fs::remove_file(&lockfile).ok();
+    std::fs::remove_file(&override_file).ok();
+}
+
+struct LocalKit;
+
+impl LocalKit {
+    fn build(registry: &KitRegistry) {
+        let local_kit = test_projects_dir().join("local-kit");
+
+        run_command(
+            TWOLITER_PATH,
+            [
+                "update",
+                "--project-path",
+                local_kit.join("Twoliter.toml").to_str().unwrap(),
+            ],
+            [],
+        );
+
+        run_command(
+            TWOLITER_PATH,
+            [
+                "fetch",
+                "--project-path",
+                local_kit.join("Twoliter.toml").to_str().unwrap(),
+            ],
+            [],
+        );
+
+        run_command(
+            TWOLITER_PATH,
+            [
+                "build",
+                "kit",
+                "core-kit",
+                "--project-path",
+                local_kit.join("Twoliter.toml").to_str().unwrap(),
+            ],
+            [],
+        );
+
+        std::fs::write(local_kit.join("Infra.toml"), INFRA_TOML).unwrap();
+        run_command(
+            TWOLITER_PATH,
+            [
+                "publish",
+                "kit",
+                "--project-path",
+                local_kit.join("Twoliter.toml").to_str().unwrap(),
+                "core-kit",
+                "bottlerocket",
+                "core-kit-overridden",
+            ],
+            [("SSL_CERT_FILE", registry.cert_file().to_str().unwrap())],
+        );
+    }
+}
+
+impl Drop for LocalKit {
+    fn drop(&mut self) {
+        let local_kit = test_projects_dir().join("local-kit");
+        std::fs::remove_file(local_kit.join("Twoliter.lock")).ok();
+        std::fs::remove_file(local_kit.join("Infra.toml")).ok();
+    }
 }
