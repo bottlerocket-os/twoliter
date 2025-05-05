dfb8887ee63323b3c0be6d3a77f163d6638cc649
Sean P. Kelly <seankell@amazon.com>
2025-05-05 23:39:05 +0000

    pubsys: upload ebs snapshots concurrently

--- tools/pubsys/src/aws/ami/register/mod.rs
+++ tools/pubsys/src/aws/ami/register/mod.rs
@@ -1,10 +1,16 @@
 use super::{snapshot::snapshot_from_image, AmiArgs};
+use crate::aws::ami::snapshot::build_progress_bar;
 use aws_sdk_ebs::Client as EbsClient;
 use aws_sdk_ec2::types::Filter;
 use aws_sdk_ec2::{config::Region, Client as Ec2Client};
 use coldsnap::{SnapshotUploader, SnapshotWaiter};
+use futures::future::OptionFuture;
+use indicatif::{MultiProgress, ProgressBar};
 use log::{debug, info, warn};
-use snafu::{ensure, OptionExt, ResultExt};
+use snafu::{ensure, futures::TryFutureExt as _, OptionExt, ResultExt, Snafu};
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 mod merge_toml;
 pub(crate) mod mk_amispec;
@@ -22,7 +28,7 @@ async fn _register_image(
     region: &Region,
     ebs_client: EbsClient,
     ec2_client: &Ec2Client,
-    cleanup_snapshot_ids: &mut Vec<String>,
+    cleanup_snapshot_ids: Arc<Mutex<Vec<String>>>,
 ) -> Result<RegisteredIds> {
     let bottlerocket_snapshots = BottlerocketSnapshots::create_snapshots(
         ami_args,
@@ -77,59 +83,105 @@ impl BottlerocketSnapshots {
     /// Creates the EBS snapshots for the OS and (optional) data volume
     ///
     /// Snapshot IDs are added to `cleanup_snapshot_ids` so that they can be deleted on cleanup.
+    async fn create_snapshot(
+        snapshot_uploader: &SnapshotUploader,
+        snapshot_waiter: &SnapshotWaiter,
+        image_path: impl AsRef<Path>,
+        cleanup_snapshot_ids: Arc<Mutex<Vec<String>>>,
+        progress_bar: Option<ProgressBar>,
+    ) -> Result<String, CreateSnapshotError> {
+        use create_snapshot_error::*;
+        let image_path = image_path.as_ref();
+
+        let snapshot_id = snapshot_from_image(image_path, snapshot_uploader, None, progress_bar)
+            .await
+            .context(UploadSnapshotSnafu { path: image_path })?;
+
+        cleanup_snapshot_ids.lock().await.push(snapshot_id.clone());
+
+        info!("Waiting for snapshot {snapshot_id} to become available");
+        snapshot_waiter
+            .wait(&snapshot_id, Default::default())
+            .await
+            .context(WaitSnapshotSnafu)?;
+
+        Ok(snapshot_id)
+    }
+
+    /// Creates the EBS snapshots for the OS and (optional) data volume
+    ///
+    /// Snapshot IDs are added to `cleanup_snapshot_ids` so that they can be deleted on cleanup.
     async fn create_snapshots(
         ami_args: &AmiArgs,
         region: &Region,
         ebs_client: EbsClient,
         ec2_client: &Ec2Client,
-        cleanup_snapshot_ids: &mut Vec<String>,
+        cleanup_snapshot_ids: Arc<Mutex<Vec<String>>>,
     ) -> Result<BottlerocketSnapshots> {
         debug!("Uploading images into EBS snapshots in {}", region);
-        let uploader = SnapshotUploader::new(ebs_client);
-        let os_snapshot =
-            snapshot_from_image(&ami_args.os_image, &uploader, None, ami_args.no_progress)
-                .await
+
+        let multi_progress = MultiProgress::new();
+
+        let create_snapshot_task = |image_path: PathBuf, image_name: &'static str| {
+            let progress_bar = (!ami_args.no_progress)
+                .then_some({
+                    let pb = build_progress_bar(&format!("Upload {image_name} snapshot"))
+                        .context(error::ProgressBarSnafu)?;
+                    Ok(multi_progress.add(pb))
+                })
+                .transpose()?;
+
+            Ok(async move {
+                info!("Registering '{image_name}' snapshot in region '{region}'");
+                let uploader = SnapshotUploader::new(ebs_client.clone());
+                let waiter = SnapshotWaiter::new(ec2_client.clone());
+                let cloned_image_path = image_path.clone();
+
+                Self::create_snapshot(
+                    &uploader,
+                    &waiter,
+                    &cloned_image_path,
+                    Arc::clone(&cleanup_snapshot_ids),
+                    progress_bar.clone(),
+                )
                 .context(error::SnapshotSnafu {
-                    path: &ami_args.os_image,
-                    region: region.as_ref(),
-                })?;
-        cleanup_snapshot_ids.push(os_snapshot.clone());
-
-        let mut data_snapshot = None;
-        if let Some(data_image) = &ami_args.data_image {
-            let snapshot = snapshot_from_image(data_image, &uploader, None, ami_args.no_progress)
+                    snapshot_type: image_name.to_string(),
+                    path: image_path,
+                    region: region.to_string(),
+                })
                 .await
-                .context(error::SnapshotSnafu {
-                    path: &ami_args.os_image,
-                    region: region.as_ref(),
-                })?;
-            cleanup_snapshot_ids.push(snapshot.clone());
-            data_snapshot = Some(snapshot);
-        }
+            })
+        };
 
-        info!("Waiting for snapshots to become available in {}", region);
-        let waiter = SnapshotWaiter::new(ec2_client.clone());
-        waiter
-            .wait(&os_snapshot, Default::default())
-            .await
-            .context(error::WaitSnapshotSnafu {
-                snapshot_type: "root",
-            })?;
+        let os_upload = create_snapshot_task.clone()(ami_args.os_image.clone(), "root")?;
 
-        if let Some(ref data_snapshot) = data_snapshot {
-            waiter
-                .wait(&data_snapshot, Default::default())
-                .await
-                .context(error::WaitSnapshotSnafu {
-                    snapshot_type: "data",
-                })?;
-        }
+        let data_upload: OptionFuture<_> = ami_args
+            .data_image
+            .as_ref()
+            .map(|data_image| create_snapshot_task(data_image.clone(), "data"))
+            .transpose()?
+            .into();
+
+        let (os_snapshot, data_snapshot) = tokio::join!(os_upload, data_upload);
+        let (os_snapshot, data_snapshot) = (os_snapshot?, data_snapshot.transpose()?);
 
         Ok(BottlerocketSnapshots {
             os_snapshot,
             data_snapshot,
         })
     }
+}
+
+#[derive(Debug, Snafu)]
+#[snafu(module)]
+pub(crate) enum CreateSnapshotError {
+    #[snafu(display("Failed to create snapshot for {}: {}", path.display(), source))]
+    UploadSnapshot {
+        path: PathBuf,
+        source: crate::aws::ami::snapshot::Error,
+    },
+    #[snafu(display("Failed to wait for snapshot to become available: {}", source))]
+    WaitSnapshot { source: coldsnap::WaitError },
 }
 
 /// Uploads the given images into snapshots and registers an AMI using them as its block device
@@ -140,18 +192,18 @@ pub(crate) async fn register_image(
     ebs_client: EbsClient,
     ec2_client: &Ec2Client,
 ) -> Result<RegisteredIds> {
-    let mut cleanup_snapshot_ids = Vec::new();
+    let cleanup_snapshot_ids = Arc::new(Mutex::new(Vec::new()));
     let register_result = _register_image(
         ami_args,
         region,
         ebs_client,
         ec2_client,
-        &mut cleanup_snapshot_ids,
+        Arc::clone(&cleanup_snapshot_ids),
     )
     .await;
 
     if register_result.is_err() {
-        for snapshot_id in cleanup_snapshot_ids {
+        for snapshot_id in cleanup_snapshot_ids.lock().await.iter() {
             if let Err(e) = ec2_client
                 .delete_snapshot()
                 .set_snapshot_id(Some(snapshot_id.clone()))
@@ -230,15 +282,13 @@ where
 }
 
 mod error {
-    use crate::aws::ami;
+    use super::{mk_amispec, CreateSnapshotError};
     use aws_sdk_ec2::error::SdkError;
     use aws_sdk_ec2::operation::{
         describe_images::DescribeImagesError, register_image::RegisterImageError,
     };
     use snafu::Snafu;
     use std::path::PathBuf;
-
-    use super::mk_amispec;
 
     #[derive(Debug, Snafu)]
     #[snafu(visibility(pub(super)))]
@@ -264,19 +314,19 @@ mod error {
             source: SdkError<RegisterImageError>,
         },
 
-        #[snafu(display("Failed to upload snapshot from {} in {}: {}", path.display(),region, source))]
-        Snapshot {
-            path: PathBuf,
-            region: String,
-            source: ami::snapshot::Error,
+        #[snafu(display("Failed to create progress bar: {}", source))]
+        ProgressBar {
+            source: crate::aws::ami::snapshot::Error,
         },
 
-        #[snafu(display("{} snapshot did not become available: {}", snapshot_type, source))]
-        WaitSnapshot {
+        #[snafu(display("Failed to upload {} snapshot from {} in {}: {}", snapshot_type, path.display(),region, source))]
+        Snapshot {
             snapshot_type: String,
-            source: coldsnap::WaitError,
+            path: PathBuf,
+            region: String,
+            source: CreateSnapshotError,
         },
     }
 }
 pub(crate) use error::Error;
-type Result<T> = std::result::Result<T, error::Error>;
+type Result<T, E = error::Error> = std::result::Result<T, E>;
